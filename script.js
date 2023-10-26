471d36987acc13edf44a5ab7c71c61d290c25d33
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2023-10-26 18:38:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let item = {
+  name:"smarphone",
+  price:50000,
+  quantity:1,
+  array:['sajin'],
+  features:{
+    processor:"snapdargon gen1"
+  }
+}
+console.log(item)
+/*
+
+{name: 'smarphone', price: 50000, quantity: 1, array: Array(1), features: {…}}
+
+*/
+  
